febf11d1c172c9c1d53b095ed768ae2a31bbbbb3
prem528 <prem05082002@gmail.com>
2024-10-04 12:09:36 +0530

    font check

--- src/Components/About.tsx
+++ src/Components/About.tsx
@@ -5,7 +5,7 @@ const About: React.FC = () => {
     <section className="mt-2 py-8 sm:py-12 pt-12 pb-0 bg-white">
       <div className="container mx-auto px-4 sm:px-6 lg:px-8 xl:px-24">
         <div className="">
-          <p className=" text-lg text-justify   mb-2 ">
+          <p className=" mb-4 text-lg sm:text-xl text-justify ">
             Delberto aims to empower artisans, crafters, aspiring entrepreneurs
             (individuals, students, retired professionals), small businesses, and marginalized communities through
             our SaaS-based technology platform. Our goal is to facilitate entrepreneurship by connecting suppliers
